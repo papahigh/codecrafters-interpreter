45f64942b77060e2c84824c3adf6fea5bed095e7
Nikola P <nikolay.papakha@gmail.com>
2025-01-13 00:31:14 +0300

    add scanner

---
+++ src/main/java/scanner/Scanner.java
@@ -0,0 +1,54 @@
+package scanner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static scanner.TokenType.*;
+
+public class Scanner {
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    public Scanner(String source) {
+        this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        while (!isEOF()) {
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(' -> addToken(LEFT_PAREN);
+            case ')' -> addToken(RIGHT_PAREN);
+        }
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        var text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean isEOF() {
+        return current >= source.length();
+    }
+}
---
+++ src/main/java/scanner/Token.java
@@ -0,0 +1,14 @@
+package scanner;
+
+public record Token(
+        TokenType type,
+        String lexeme,
+        Object literal,
+        int line
+) {
+
+    @Override
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
